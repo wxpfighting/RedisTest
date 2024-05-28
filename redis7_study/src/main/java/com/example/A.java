5b0b3c603f61a946fb20397869eee3a32524b481
wxpfighting <2788563974@qq.com>
2024-05-28 21:13:41 +0800

    Update A.java

--- redis7_study/src/main/java/com/example/A.java
+++ redis7_study/src/main/java/com/example/A.java
@@ -6,6 +6,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("ok");
         System.out.println("masterr");
+        System.out.println("ok");
         System.out.println("hot-fix");
         System.out.println("hhhot-fix");System.out.println("ok");
         System.out.println("ok");
