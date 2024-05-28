1e51be683788a81196c62a1d6dfb7b52f04b94e9
wxpfighting <2788563974@qq.com>
2024-05-28 19:35:29 +0800

    master first change

--- redis7_study/src/main/java/com/example/A.java
+++ redis7_study/src/main/java/com/example/A.java
@@ -5,6 +5,6 @@ public class A {
 
     public static void main(String[] args) {
         System.out.println("ok");
-        System.out.println("master");
+        System.out.println("masterr");
     }
 }
