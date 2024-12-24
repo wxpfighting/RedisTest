f539681156f536db329cd1965459872f080f455b
wxpfighting <2788563974@qq.com>
2024-12-24 21:44:37 +0800

    Update Redis7StudyApplication.java

--- redis7_study/src/main/java/com/example/Redis7StudyApplication.java
+++ redis7_study/src/main/java/com/example/Redis7StudyApplication.java
@@ -13,7 +13,7 @@ import java.util.*;
 public class Redis7StudyApplication {
 
     public static void main(String[] args) {
-        System.out.println("容器开始加载7");
+        System.out.println("容器开始加载71");
         SpringApplication.run(Redis7StudyApplication.class, args);
     }
 
